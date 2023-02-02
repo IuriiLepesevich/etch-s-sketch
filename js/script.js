3b457e78e7ae5eef034141849e11b8870e9166b6
IuriiLepesevich <iurii.lepesevich@gmail.com>
2023-02-02 18:42:48 +0200

    Add ability for user to determine size of grid

--- js/script.js
+++ js/script.js
@@ -1,13 +1,54 @@
 const grid = document.querySelector('#grid');
-const grid_width = grid.offsetWidth;
-
 const numberOfSquares = 16;
+const button = document.querySelector('button');
+const inputField = document.querySelector('input');
 
-for(let i = 0; i < numberOfSquares**2; i++) {
-    let grid_elem = document.createElement('div');
-    grid_elem.classList.add('grid-elem');
-    grid_elem.style.width = `${grid_width / numberOfSquares}px`;
-    grid_elem.style.height = `${grid_width / numberOfSquares}px`;
 
-    grid.appendChild(grid_elem);
+inputField.style.color = 'gray';
+placeholder = 'Number of squares per row';
+inputField.value = placeholder;
+
+inputField.addEventListener('focus', function () {
+    if (this.value === placeholder) {
+        this.style.color = 'black';
+        this.value = '';
+    }
+});
+inputField.addEventListener('blur', function () {
+    if (this.value === '') {
+        this.style.color = 'gray';
+        this.value = placeholder;
+    }
+});
+
+function drawGrid(num) {
+    const grid_width = grid.offsetWidth;
+
+    for(let i = 0; i < num**2; i++) {
+        let grid_elem = document.createElement('div');
+        grid_elem.classList.add('grid-elem');
+        grid_elem.style.width = `${grid_width / num}px`;
+        grid_elem.style.height = `${grid_width / num}px`;
+
+        grid.appendChild(grid_elem);
+    }
 }
+
+function clearGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+clearGrid();
+drawGrid(numberOfSquares);
+
+button.addEventListener('click', () => {
+    inputFieldValue = +inputField.value;
+
+    if(!inputFieldValue || !Number.isInteger(inputFieldValue)) return;
+
+    clearGrid();
+    drawGrid((inputFieldValue > 100) ? 100 : inputFieldValue);
+
+})
